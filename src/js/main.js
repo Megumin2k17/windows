c8bc69be5e5536527200aedcb014a4b6e8572ad5
unknown <nether@list.ru>
2023-09-28 21:46:03 +0300

    Add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 import "./slider.js";
 import modals from "./modules/modals.js";
-modals();
+import tabs from "./modules/tabs3.js";
+
 console.log("Hello World!");
+
+window.addEventListener("DOMContentLoaded", () => {
+	modals();
+
+	tabs(".glazing_slider", ".glazing_block", "active", ".glazing_content");
+	tabs(
+		".decoration_slider",
+		".no_click",
+		"after_click",
+		".decoration_content > div > div"
+	);
+});
---
+++ src/js/modules/tabs3.js
@@ -0,0 +1,63 @@
+const tabs = (
+	tabsContainerSelector,
+	tabsSelector,
+	activeTabClass,
+	contentsContainersSelector
+) => {
+	const tabsContainer = document.querySelector(tabsContainerSelector);
+	const tabs = document.querySelectorAll(tabsSelector);
+	const tabsContents = document.querySelectorAll(contentsContainersSelector);
+
+	let currentTab = 0;
+	/* 
+		В общем такой вот варик выглядит наименее каличным с точки зрения визуала на страничке
+		В данном варике будет забаговано только отображение активного таба
+		Если убрать bindTabsData(dataset'ы), то вкладки перестанут нормально переключаться		
+		Судя по всему это происходит потому что идиотский слайдер клонирует табы и свопает их туда-сюда
+		По-моему сделать по-человечески тут не получится из-за вот этого вот слайдера
+		На десткоп версии более менее работает, а вот когда слайдер появляется - будет всё к писюнам забаговано 
+		У автора видоса вообще будет всегда багованый дополнительный активный таб и не будет адекватно контент переключаться
+		Крипота .-.
+	*/
+	const bindTabsData = () => {
+		tabs.forEach((tab, idx) => {
+			tab.dataset["tabId"] = idx;
+			tabsContents[idx].dataset["tabId"] = idx;
+		});
+	};
+
+	const showTab = (idx) => {
+		tabs[idx].classList.add(activeTabClass);
+
+		tabsContents[idx].style.display = "block";
+	};
+
+	const closeTabs = () => {
+		// Это чтобы убрать дублированую активную вкладку от клонов ссаного слайдера
+		const tabs = tabsContainer.querySelectorAll(tabsSelector);
+		// костыли
+		tabs.forEach((tab) => {
+			tab.classList.remove(activeTabClass);
+		});
+
+		tabsContents.forEach((tabContent) => {
+			tabContent.style.display = "none";
+		});
+	};
+
+	bindTabsData();
+	closeTabs();
+	showTab(currentTab);
+
+	tabsContainer.addEventListener("click", (e) => {
+		const tab = e.target.closest(tabsSelector);
+
+		if (tab) {
+			currentTab = tab.dataset.tabId;
+			closeTabs();
+			showTab(currentTab);
+		}
+	});
+};
+
+export default tabs;
