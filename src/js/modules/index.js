2343b19b5ea603844aae434cfac40f10b2b34203
unknown <nether@list.ru>
2023-10-22 11:43:56 +0300

    Add image preview

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -2,4 +2,5 @@ export { modals } from "./modals.js";
 export { tabs } from "./tabs3.js";
 export { forms } from "./forms.js";
 export { timer } from "./timer.js";
+export { images } from "./images.js";
 export { changeModalState } from "./changeModalState.js";
